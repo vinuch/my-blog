8a7158fcf931484c9311fbadb787c18eb8c16769
vinuch <vincentedeh42@gmail.com>
2021-06-01 10:28:20 -0700

    feat: add resume link to navigation

--- src/layouts/partials/Header.vue
+++ src/layouts/partials/Header.vue
@@ -2,7 +2,7 @@
   <header>  
     
     <div class="flex w-full relative text-gray-400 mx-auto bg-black px-4 sm:px-0">
-      <div class="container w-full mx-auto flex flex-wrap">  
+      <div class="container w-full mx-auto flex items-center flex-wrap">  
         <div class="w-full md:w-6/12 sm:w-3/12 lg:w-2/12 my-5 mx-2">
         <a href="/">
           <span>
@@ -18,7 +18,7 @@
           </a>
         </div>
         <div class="w-full md:w-5/12 md:w-9/12">
-          <div class="flex-1 text-gray-500 text-xs text-center mb-6 pt-1">
+          <div class="flex-1 text-gray-500 text-xs text-center  pt-1">
             <HeaderNavigation/>
           </div>
         </div>
